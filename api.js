76df06f423a63eecd5353b4fae0e255afc0ef14a
ProgradeX <mtayyab5402@gmail.com>
2022-06-12 17:00:58 +0500

    base api complete

--- api.js
+++ api.js
@@ -2,6 +2,60 @@ var Db = require('./dboperations');
 var Order = require('./orders');
 const dboperations = require('./dboperations');
 
-dboperations.getOrders().then(result => {
-    console.log(result);
+// requiring stuffs
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+const { response } = require('express');
+var app = express();
+var router = express.Router();
+
+// configuring stuffs
+app.use(bodyParser.urlencoded({ extended:  true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);
+
+//creating middleware
+router.use((requrest, response, next) => {
+    console.log('middleware mmmm')
+    next();
 })
+
+router.route('/orders').get((request, response) => {
+    dboperations.getOrders().then(result => {
+        //console.log(result);
+        response.json(result[0]);
+    })
+})
+
+router.route('/orders/:id').get((request, response) => {
+    dboperations.getOrder(request.params.id).then(result => {
+        console.log(typeof result);
+        response.json(result[0]);
+    })
+})
+
+router.route('/orders').post((request, response) => {
+
+    let order = {...request.body}
+
+    dboperations.addOrder(order).then(result => {
+        response.status(201).json(result);
+    })
+})
+
+router.route('/orders/delete/:id').post((request, response) => {
+    dboperations.delOrder(request.params.id).then(result => {
+        console.log('this working maybe?')
+        response.json(result[1]);
+    })
+})
+
+
+// listening on port
+var  port = process.env.PORT || 8090;
+app.listen(port);
+console.log('Order API is runnning at ' + port);
+
+
--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,20 @@
 const  config = {
+    //user: 'CODECRACKER-500\\Prograde',
+    //password: '',
     server:  '192.168.18.12', // if it does not work try- localhost
     database:  'Products',
+
+    user:'prograde',
+    password:'bruh',
+    //domain:'CODECRACKER-500\\Prograde',
+
+    driver: "msnodesqlv8",
+
     options: {
-      trustedconnection:  true,
+      //trustedconnection:  true,
+      trustServerCertificate: true,
       enableArithAbort:  true,
-      instancename:  'SQLEXPRESS'  // SQL Server instance name
+      instancename:  'CODECRACKER-500'  // SQL Server instance name
     },
     port: 1433
   }
